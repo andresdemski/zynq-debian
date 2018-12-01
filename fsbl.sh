af8b44089dce1db3247937b3d7a64ec9a95e4d3e
ademski <ademski@satellogic.com>
2018-12-01 17:33:50 -0300

    reusing code

--- fsbl.sh
+++ fsbl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 SRC_PATH=$PWD/srcs/embeddedsw
-DOCKERFILE_DIR=.files/
-DOCKERFILE=Dockerfile.build
-BUILD_IMG=zynq_build_image
-MAKE_ARGS=" -C./lib/sw_apps/zynq_fsbl/src/ BOARD=zed CC=arm-none-eabi-gcc SHELL=/bin/bash"
+
+DOCKER_IMG=zynq_build_image
 DOCKER_ARGS="-it --rm -v$PWD:$PWD -w $SRC_PATH -u $(id -u):$(id -g)"
+
+MAKE_ARGS=" -C./lib/sw_apps/zynq_fsbl/src/ BOARD=zed CC=arm-none-eabi-gcc SHELL=/bin/bash"
 
 mkdir -p $PWD/srcs
 [[ -d $SRC_PATH ]] || ( git clone https://github.com/Xilinx/embeddedsw $SRC_PATH && \
@@ -16,12 +16,8 @@ mkdir -p $PWD/srcs
 # Patch reason: *.o uses VFP register arguments, fsbl.elf does not
 # It's not a good solution but it works :D
 
-cd $DOCKERFILE_DIR
-docker build -f $DOCKERFILE -t $BUILD_IMG .
-cd -
-
-
-docker run $DOCKER_ARGS $BUILD_IMG make $MAKE_ARGS
+./build_docker.sh
+docker run $DOCKER_ARGS $DOCKER_IMG make $MAKE_ARGS
 
 mkdir -p outputs
 mv $SRC_PATH/lib/sw_apps/zynq_fsbl/src/fsbl.elf outputs/fsbl.elf
--- uboot.sh
+++ uboot.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 set -ex
 SRC_PATH=$PWD/srcs/uboot
-DOCKERFILE_DIR=.files
-DOCKERFILE=Dockerfile.build
-BUILD_IMG=zynq_build_image
+
+DOCKER_IMG=zynq_build_image
+DOCKER_ARGS="-it --rm -v$PWD:$PWD -w $SRC_PATH -u $(id -u):$(id -g)"
+
 MAKE_ARGS="ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- DTC_FLAGS=--symbols"
 UBOOT_DEFCONFIG=zynq_zybo_defconfig
+
 UENV_TEMPLATE=.files/uEnv.txt.template
 TEMPLATE_CMD="sed 's/{% kernel %}/zImage/' $UENV_TEMPLATE | \
               sed 's/{% devicetree %}/zync-zybo.dtb/'"
 
-DOCKER_ARGS="-it --rm -v$PWD:$PWD -w $SRC_PATH -u $(id -u):$(id -g)"
 
 mkdir -p $PWD/srcs
 [[ -d $SRC_PATH ]] || git clone https://github.com/Xilinx/u-boot-xlnx.git $SRC_PATH
 
-cd $DOCKERFILE_DIR
-docker build -f $DOCKERFILE -t $BUILD_IMG .
-cd -
+./build_docker.sh
 
-docker run $DOCKER_ARGS $BUILD_IMG make $MAKE_ARGS $UBOOT_DEFCONFIG
-docker run $DOCKER_ARGS $BUILD_IMG make $MAKE_ARGS
+docker run $DOCKER_ARGS $DOCKER_IMG make $MAKE_ARGS $UBOOT_DEFCONFIG
+docker run $DOCKER_ARGS $DOCKER_IMG make $MAKE_ARGS
 
 mkdir -p outputs
 mv $SRC_PATH/u-boot outputs/uboot.elf
